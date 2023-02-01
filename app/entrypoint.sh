20a13ea85c9931dec4805468a7c3dc7b30865c7d
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-02-01 17:33:40 +0000

    Fixes some enviromment and dependency issues

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,11 +5,21 @@ mkdir -p /app/_output
 
 TARGET_NOTEBOOK=${TARGET_NOTEBOOK:-"Create Cites from PMC Lookups - Monthly.ipynb"}
 
+# debugging
+echo "--- Starting the PMC crawler with the following parameters:"
+echo "* START_DATE: ${START_DATE}"
+echo "* END_DATE: ${END_DATE}"
+echo "* AUTHORS_SHEET_ID: ${AUTHORS_SHEET_ID}"
+echo "* DEPARTMENT: ${DEPARTMENT}"
+echo "---"
+
 cd /app/notebooks && \
 poetry run papermill \
     --no-report-mode \
     --log-output \
-    -r start_date ${START_DATE} \
-    -r end_date ${END_DATE} \
+    -r start_date "${START_DATE}" \
+    -r end_date "${END_DATE}" \
+    -r authors_sheet_id "${AUTHORS_SHEET_ID}" \
+    -r department "${DEPARTMENT}" \
     "${TARGET_NOTEBOOK}" \
     "/app/_output/${TARGET_NOTEBOOK}"  
