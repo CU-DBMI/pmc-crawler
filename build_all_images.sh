aaeb921d3810cf5a21ed4633b71ca9f18f8985b9
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-02-09 15:22:04 +0000

    Build script now builds for linux/arm64,amd64, pushes to GCR

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-docker build -t pmc-crawler:latest ./app
+IMAGE="pmc-crawler:latest"
+GCP_IMAGE=${GCP_IMAGE:-"us-central1-docker.pkg.dev/cuhealthai-foundations/tools/${IMAGE}"}
+
+# builds the image locally as $IMAGE
+# also attempts to push it to our remote artifact registry, or wherever $GCP_IMAGE points
+docker buildx create --name pmc-context --use 
+docker buildx build --platform=linux/amd64,linux/arm64 -t ${IMAGE} ./app && (
+    [ ! -z "${GCP_IMAGE}" ] \
+        && ( docker tag ${IMAGE} ${GCP_IMAGE} && docker push ${GCP_IMAGE} ) \
+        || echo "GCP_IMAGE unspecified, leaving image as local ${IMAGE}"
+)
+# clean up the build context when we're done
+docker buildx rm pmc-context
