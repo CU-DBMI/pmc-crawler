aa45924d50aa463ba77c367af7a8821fdcb66e3a
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-06-12 19:32:34 -0600

    build_all_images now always tags images w/GCP prefix, even when not pushing. added some output about the results of the build.

--- build_all_images.sh
+++ build_all_images.sh
@@ -11,14 +11,25 @@ DO_IMAGE_PUSH='0'
 (
     if [ "${MULTI_PLATFORM_BUILD}" = "1" ]; then
         docker buildx create --name pmc-context --use 
-        docker buildx build --platform=linux/amd64,linux/arm64 -t ${IMAGE} ./app
+        docker buildx build --platform=linux/amd64,linux/arm64 -t ${IMAGE} ./app && \
+        docker tag ${IMAGE} ${GCP_IMAGE}
         # clean up the build context when we're done
         # docker buildx rm pmc-context
     else
-        docker build -t ${IMAGE} ./app
+        docker build -t ${IMAGE} ./app && \
+        docker tag ${IMAGE} ${GCP_IMAGE}
     fi
+
+    # echo out the tags for the image
+    echo
+    echo "Built image:"
+    echo "  ${IMAGE}"
+    echo "Addt'l tag for remote registry:"
+    echo "  ${GCP_IMAGE}"
+    echo ""
+
 ) && (
     ( [ ! -z "${GCP_IMAGE}" ] && [ "${DO_IMAGE_PUSH}" = "1" ] ) \
-        && ( docker tag ${IMAGE} ${GCP_IMAGE} && docker push ${GCP_IMAGE} ) \
-        || echo "GCP_IMAGE or DO_IMAGE_PUSH disabled unspecified, leaving image as local ${IMAGE}"
+        && docker push ${GCP_IMAGE} \
+        || echo "GCP_IMAGE or DO_IMAGE_PUSH disabled unspecified, not pushing to remote"
 )
